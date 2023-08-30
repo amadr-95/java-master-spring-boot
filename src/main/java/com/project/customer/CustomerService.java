956b23121c81fb40348af8b60c62c830bb67ee62
Amador <amasabcar@outlook.com>
2023-08-30 15:21:59 +0200

    fix getAllCustomers method name

--- src/main/java/com/project/customer/CustomerService.java
+++ src/main/java/com/project/customer/CustomerService.java
@@ -15,7 +15,7 @@ public class CustomerService {
     }
 
     public List<Customer> getAllCostumers() {
-        return customerDAO.getAllCostumers();
+        return customerDAO.getAllCustomers();
     }
 
     public Customer findById(int customerId) {
