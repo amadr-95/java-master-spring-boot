64de8dfe903106b0d825bd20b2289e719f829b8a
Amador <amasabcar@outlook.com>
2023-08-30 13:57:48 +0200

    added saveCustomer endpoint and url refactored

--- src/main/java/com/project/customer/CustomerController.java
+++ src/main/java/com/project/customer/CustomerController.java
@@ -2,11 +2,15 @@ package com.project.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -15,13 +19,18 @@ public class CustomerController {
     }
 
     //@GetMapping("api/v1/customer") = @RequestMapping(path = "api/v1/customer", method = RequestMethod.GET)
-    @GetMapping("api/v1/customers")
+    @GetMapping
     public List<Customer> getAllCostumers() {
         return customerService.getAllCostumers();
     }
 
-    @GetMapping("api/v1/customers/{customerId}")
+    @GetMapping("{customerId}")
     public Customer findById(@PathVariable(name = "customerId") int customerId) {
         return customerService.findById(customerId);
+    }
+
+    @PostMapping
+    public void saveCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
     }
 }
