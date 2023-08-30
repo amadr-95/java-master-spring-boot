2ff6a430728ddcb190c4bdf0594b2b27f0f9e91a
Amador <amasabcar@outlook.com>
2023-08-30 12:11:26 +0200

    added JPA implementation

---
+++ src/main/java/com/project/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,30 @@
+package com.project.customer;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+//@Primary
+@Qualifier("jpa")
+public class CustomerJPADataAccessService implements CustomerDAO {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCostumers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(int id) {
+        return Optional.empty();
+    }
+}
