c11e4debf990f5bd3758a43ee2138211f003a7ef
Amador <amasabcar@outlook.com>
2023-08-30 13:22:39 +0200

    added saveCustomer method

--- src/main/java/com/project/customer/CustomerService.java
+++ src/main/java/com/project/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package com.project.customer;
 
-import com.project.exception.ResourceNotFound;
+import com.project.exception.DuplicateResourceException;
+import com.project.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,13 @@ public class CustomerService {
 
     public Customer findById(int customerId) {
         return customerDAO.findById(customerId)
-                .orElseThrow(() -> new ResourceNotFound("customer with id [%s] does not exist".formatted(customerId)));
+                .orElseThrow(() -> new ResourceNotFoundException("customer with id [%s] does not exist".formatted(customerId)));
+    }
+
+    public void saveCustomer(Customer customer) {
+        //check if email already exist
+        if(customerDAO.existCustomerByEmail(customer.getEmail()))
+            throw new DuplicateResourceException("email already exist");
+        customerDAO.saveCustomer(customer);
     }
 }
