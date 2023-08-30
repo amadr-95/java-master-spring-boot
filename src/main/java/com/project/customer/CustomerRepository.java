4c57e6c07c0285e7e44c7d44ebb628ca4683dfbb
Amador <amasabcar@outlook.com>
2023-08-30 13:54:16 +0200

    added query annotation to check if email already exist

--- src/main/java/com/project/customer/CustomerRepository.java
+++ src/main/java/com/project/customer/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.project.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 //no need to be annotated with @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Customer c WHERE c.email = ?1")
     boolean existCustomerByEmail(String email);
 }
