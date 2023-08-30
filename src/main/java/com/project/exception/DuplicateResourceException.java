cc0ba0c7e9ff12c795cf40dc6eaad478fe7169e7
Amador <amasabcar@outlook.com>
2023-08-30 13:21:19 +0200

    added new custom exception to check duplicate resources

---
+++ src/main/java/com/project/exception/DuplicateResourceException.java
@@ -0,0 +1,26 @@
+package com.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException() {
+    }
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateResourceException(Throwable cause) {
+        super(cause);
+    }
+
+    public DuplicateResourceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
