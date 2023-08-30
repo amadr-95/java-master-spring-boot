82371d9394cddf92eb02d05ca389068b70aa5753
Amador <amasabcar@outlook.com>
2023-08-30 12:10:57 +0200

    added comment

--- src/main/java/com/project/customer/CustomerRepository.java
+++ src/main/java/com/project/customer/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.project.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//no need to be annotated with @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 }
