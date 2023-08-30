4d8467b5fa7ecf5d1ea708e6c82fef16e485ab55
Amador <amasabcar@outlook.com>
2023-08-30 16:14:29 +0200

    added custom exception to check if any data has changed when updating a customer

---
+++ src/main/java/com/project/exception/RequestValidationException.java
@@ -0,0 +1,27 @@
+package com.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class RequestValidationException extends RuntimeException {
+
+    public RequestValidationException() {
+    }
+
+    public RequestValidationException(String message) {
+        super(message);
+    }
+
+    public RequestValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RequestValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public RequestValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
